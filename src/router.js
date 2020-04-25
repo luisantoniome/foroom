e6ce92c3de029a05a68b67c941e0a1ba77770624
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2020-04-25 06:44:00 -0500

    NotFound view and route created

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import ThreadShow from './views/ThreadShow.vue'
+import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -19,6 +20,11 @@ export default new Router({
       name: 'ThreadShow',
       component: ThreadShow,
       props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
